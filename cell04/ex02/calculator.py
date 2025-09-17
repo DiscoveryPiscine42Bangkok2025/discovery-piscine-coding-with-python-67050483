b15abe92f24aa50d02b4e377b0474e9bef197d1a
67050483 <67050483@kmitl.ac.th>
2025-09-17 19:25:45 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+first_num = int(input("Give me the first number : "))
+second_num = int(input("Give me the second number : "))
+
+print("Thanks you")
+
+print(str(first_num) + " + " + str(second_num) + " = " + str(first_num + second_num))
+print(str(first_num) + " - " + str(second_num) + " = " + str(first_num - second_num))
+print(str(first_num) + " / " + str(second_num) + " = " + str(first_num / second_num))
+print(str(first_num) + " * " + str(second_num) + " = " + str(first_num * second_num))
