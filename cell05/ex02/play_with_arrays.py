34f8161000d8642ef6174e763cd03341b1d6f5ca
67050483 <67050483@kmitl.ac.th>
2025-09-18 22:44:44 +0700

    Update play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,5 +1,5 @@
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = [i + 2 for i in original_array if i + 2 > 5]
+new_array = [i + 2 for i in original_array if i > 5]
     
 print(original_array)
 print(new_array)
