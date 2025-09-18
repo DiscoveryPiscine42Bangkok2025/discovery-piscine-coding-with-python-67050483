18d48adee67ef8c2c12c3be6fb132df65ebd716f
67050483 <67050483@kmitl.ac.th>
2025-09-18 23:15:20 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,6 +1,12 @@
-your_word = input("What was the parameter? : ")
+import sys
 
-if your_word != "Hello" :
-    print("Nope, sorry...")
-else :
-    print("Good job!")
+if len(sys.argv) != 2:
+    print("none")
+else:
+    param = sys.argv[1]
+    your_word = input("What was the parameter? : ")
+
+    if your_word != param:
+        print("Nope, sorry...")
+    else:
+        print("Good job!")
